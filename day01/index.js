f84efd8e3d23d6c16b34959deacc6bb59e500b6d
Oscar Heimdahl <o.heimdahl@gmail.com>
2021-11-30 17:27:51 +0100

    prep

--- day01/index.js
+++ day01/index.js
@@ -1,14 +1,30 @@
+const fs = require('fs');
+
+function inputDataLinesIntegers(filename = 'input.txt') {
+  return fs
+    .readFileSync(filename)
+    .toString()
+    .trim()
+    .split('\n')
+    .map((x) => parseInt(x));
+}
+
 function getSolutionPart1() {
-    return 1337;
+  return inputDataLinesIntegers().reduce((x, y) => x + y);
 }
 
 function getSolutionPart2() {
-    return 42;
+  return inputDataLinesIntegers().reduce((x, y) => x * y);
 }
 
-const part = process.env.part || "part1";
+console.log('Javascript');
+const part = process.env.part || 'part1';
 
-if (part === "part1")
-    console.log(getSolutionPart1());
-else
-    console.log(getSolutionPart2());
+if (part === 'part1') console.log(getSolutionPart1());
+else console.log(getSolutionPart2());
+
+module.exports = {
+  getSolutionPart1,
+  getSolutionPart2,
+  inputDataLinesIntegers,
+};
